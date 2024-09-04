0ad18726082bf866baf97b8b842dcdb7128956e9
alvinyanson <a.yanson@arcanys.com>
2024-09-04 14:33:31 +0800

    feat: add and remove claims

---
+++ ASPNETCoreIdentityDemo/Models/UserClaimsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ASPNETCoreIdentityDemo.Models
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            //To Avoid runtime exception, we are initializing the Claims property
+            Claims = new List<UserClaim>();
+        }
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+    }
+}
