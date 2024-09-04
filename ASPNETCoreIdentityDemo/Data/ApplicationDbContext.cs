f28f1f771654704ed415f03a9ec3ca503781812b
alvinyanson <a.yanson@arcanys.com>
2024-09-04 13:53:56 +0800

    feat: enforce on delete no action in user roles

--- ASPNETCoreIdentityDemo/Data/ApplicationDbContext.cs
+++ ASPNETCoreIdentityDemo/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ASPNETCoreIdentityDemo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,6 +10,22 @@ namespace ASPNETCoreIdentityDemo.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customizing the ASP.NET Identity model and overriding the defaults if needed
+            builder.Entity<IdentityUserRole<string>>()
+                   .HasOne<ApplicationRole>()
+                   .WithMany()
+                   .HasForeignKey(ur => ur.RoleId)
+                   .OnDelete(DeleteBehavior.NoAction);
+            //The following code will set ON DELETE NO ACTION to all Foreign Keys
+            //foreach (var foreignKey in builder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            //{
+            //    foreignKey.DeleteBehavior = DeleteBehavior.NoAction;
+            //}
         }
     }
 }
